2149e7c7d742b455b4a45cb18bb8f1e8eda80705
InfiniteGreen <46692630+InfiniteGreen@users.noreply.github.com>
2019-06-08 10:44:28 -0400

    finished option 1 for showing general fx chart + info, part of option 2 done showing 2016 fx chart with trading data

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,10 @@
+// from data.js
+var tradeData = data;
+
+// Create empty arrays to store info
+var url = [];
+var filteredData = [];
+
 function handleSubmit() {
 
   // Prevent the page from refreshing
@@ -6,7 +13,7 @@ function handleSubmit() {
   // Select the input value from the form
   var fx = d3.select("#fxInput").node().value;
   var ao = d3.select("#analysis_option").node().value; 
-  var url = [];
+  
   // console.log(fx);
   // console.log(ao);
 
@@ -44,12 +51,47 @@ function prepUrl(fx,ao){
   // console.log(url);
 };
 
-function handleTradeData (fx, ao) {
+function handleTradeData (fx, ao) {   
+  var inputFXSymbol = fx.replace(/_/g, '/');
+  // var tradeSymbol = [];
+  var tradeOpenDate = [];
+  var tradeCloseDate = [];
+  var tradeOpenPrice = [];
+  var tradeClosePrice = []; 
+  var NetPnL = [];
 
+  // Create an array with the filtered values
+  filteredData = tradeData.filter(trade => {        
+    if (inputFXSymbol === trade.Symbol){
+      return true;
+    }
+    else {
+      return false;
+    };
+  }); 
+  console.log(filteredData);
+  // console.log(Object.keys(filteredData));
+
+  for (var i=0; i<filteredData.length; i++){
+    // console.log(Object.keys(filteredData[i]));
+    // console.log(filteredData[i]["Date Open"]);
+    tradeOpenDate.push(filteredData[i]["Date Open"]);
+    tradeCloseDate.push(filteredData[i]["Date Close"]);
+    tradeOpenPrice.push(filteredData[i]["Bought"]);
+    tradeClosePrice.push(filteredData[i]["Sold"]);
+    NetPnL.push(filteredData[i]["Net P/L"]);
+  };
+  console.log(tradeOpenDate);
+ 
 };
 
+
 function buildPlot(fx, ao) {
-  // console.log(url);
+  var trace1 = {};
+  var trace2 = {};
+  var data = [];
+  var layout = {};
+  
   d3.json(url).then(function(response) {
     // console.log(response);  
     var name = response.instrument;     
@@ -66,57 +108,185 @@ function buildPlot(fx, ao) {
       lowMid.push(response.candles[i].lowMid);
       openMid.push(response.candles[i].openMid);
       time.push(response.candles[i].time.substring(0,10));
-      // console.log(time);
-    };
+    };  
 
-    // console.log(time);
-    var trace1 = {
-      type: "scatter",
-      mode: "lines",
-      name: name,
-      x: time,
-      y: closeMid,
-      line: {
-        color: "blue"
-      }
-    };
+  switch (ao) {
+    //API with last 365 days of fx data  
+    case "fx_general":
+      trace1 = {
+        type: "scatter",
+        mode: "lines",
+        name: name,
+        x: time,
+        y: closeMid,
+        line: {
+          color: "blue"
+        }
+      };
+      data = [trace1];
+      layout = {
+        title: `${fx} closing prices`,
+        xaxis: {
+          range: [time[0], time[-1]],
+          type: "date"
+        },
+      };
+      break;
 
-  //   // Candlestick Trace
-  //   var trace2 = {
-  //     type: "candlestick",
-  //     x: dates,
-  //     high: highPrices,
-  //     low: lowPrices,
-  //     open: openingPrices,
-  //     close: closingPrices
-  //   };
+    //Graph general 2016 chart with Rectangle trade data 
+    case "trade_fx":
+      
+      // trace1 = {
+      //   type: "scatter",
+      //   mode: "lines",
+      //   name: name,
+      //   x: time,
+      //   y: closeMid,
+      //   line: {
+      //     color: "blue"
+      //   }
+      // };
+      // Candlestick Trace
+      // trace2 = {
+      //   type: "candlestick",
+      //   x: dates,
+      //   high: highPrices,
+      //   low: lowPrices,
+      //   open: openingPrices,
+      //   close: closingPrices
+      // };
 
-  //   var data = [trace1, trace2];
-  var data = [trace1];
 
-  //   var layout = {
-  //     title: `${stock} closing prices`,
-  //     xaxis: {
-  //       range: [startDate, endDate],
-  //       type: "date"
-  //     },
-  //     yaxis: {
-  //       autorange: true,
-  //       type: "linear"
-  //     }
-  //   };
+      var inputFXSymbol = fx.replace(/_/g, '/');
+      // var tradeSymbol = [];
+      var tradeOpenDate = [];
+      var tradeCloseDate = [];
+      var tradeOpenPrice = [];
+      var tradeClosePrice = []; 
+      var NetPnL = [];
 
-     var layout = {
-      title: `${fx} closing prices`,
-      xaxis: {
-        range: [time[0], time[-1]],
-        type: "date"
-      },
-      // yaxis: {
-      //   autorange: true,
-      //   type: "linear"
-      // }
-    };
+      // Create an array with the filtered values
+      filteredData = tradeData.filter(trade => {        
+        if (inputFXSymbol === trade.Symbol){
+          return true;
+        }
+        else {
+          return false;
+        };
+      }); 
+      console.log(filteredData);
+      // console.log(Object.keys(filteredData));
+
+      for (var i=0; i<filteredData.length; i++){
+        // console.log(Object.keys(filteredData[i]));
+        // console.log(filteredData[i]["Date Open"]);
+        tradeOpenDate.push(filteredData[i]["Date Open"]);
+        tradeCloseDate.push(filteredData[i]["Date Close"]);
+        tradeOpenPrice.push(filteredData[i]["Bought"]);
+        tradeClosePrice.push(filteredData[i]["Sold"]);
+        NetPnL.push(filteredData[i]["Net PL"]);
+      };
+      
+      console.log(tradeOpenDate);
+      var trace1 = {
+        x: time,
+        y: closeMid
+        // text: ['Unfilled Rectangle', 'Filled Rectangle'],
+        // mode: 'text'
+      };
+      
+      data = [trace1];
+      var layout = {
+        title: `${fx} Trade Rectangle Positioned Relative to the Axes`,
+        xaxis: {
+          range: [time[0], time[-1]],
+          // showgrid: true
+        },
+        yaxis: {
+          range: [closeMid[0], closeMid[-1]],
+        },
+        shapes: [
+      
+          //Unfilled Rectangle
+      
+          // {
+          //   type: 'rect',
+          //   x0: 1,
+          //   y0: 1,
+          //   x1: 2,
+          //   y1: 3,
+          //   line: {
+          //     color: 'rgba(128, 0, 128, 1)'
+          //   }
+          // },
+         
+          //Filled Rectangle      
+          {
+            type: 'rect',
+            x0: tradeOpenDate[0],
+            y0: tradeOpenPrice[0],
+            x1: tradeCloseDate[0],
+            y1: tradeClosePrice[0],
+            line: {
+              color: 'rgba(128, 0, 128, 1)',
+              width: 2
+            },
+            fillcolor: 'rgba(128, 0, 128, 0.7)'
+          },
+          {
+            type: 'rect',
+            x0: tradeOpenDate[1],
+            y0: tradeOpenPrice[1],
+            x1: tradeCloseDate[1],
+            y1: tradeClosePrice[1],
+            line: {
+              color: 'rgba(128, 0, 128, 1)',
+              width: 2
+            },
+            fillcolor: 'rgba(128, 0, 128, 0.7)'
+          },
+        ]
+      };
+      
+      // data = [trace1, trace2];
+      // layout = {
+      //   title: `${fx} closing prices`,
+      //   xaxis: {
+      //     range: [time[0], time[-1]],
+      //     type: "date"
+      //   },
+      //   yaxis: {
+      //     autorange: true,
+      //     type: "linear"
+      //   }
+      // };
+      break;
+    //API for 2016 fx data 
+    case "trade_analysis":
+      handleTradeData (fx, ao);
+      trace1 = {
+        type: "scatter",
+        mode: "lines",
+        name: name,
+        x: time,
+        y: closeMid,
+        line: {
+          color: "blue"
+        }
+      };
+      data = [trace1];
+      layout = {
+        title: `${fx} Trade`,
+        xaxis: {
+          range: [time[0], time[-1]],
+          type: "date"
+        },
+      };
+      break;
+    //API with last 365 days of fx data   
+    default:
+      url = `https://api-fxtrade.oanda.com/v1/candles?instrument=${fx}&candleFormat=midpoint&count=365&granularity=D`;
+    }
 
     Plotly.newPlot("plot", data, layout);
   });
@@ -137,10 +307,9 @@ function renderTable(fx) {
     thead.append("th").text(col);
   });
   
-  //Populate table body
-  
+  //Populate table body  
   d3.json(url).then(function(response) {
-    console.log(response);  
+    // console.log(response);  
 
     var tbody = d3.select("tbody");
             
@@ -156,14 +325,11 @@ function renderTable(fx) {
       row.append("td").text(response.candles[r].lowMid);
       row.append("td").text(response.candles[r].closeMid);
 
-      console.log(response.candles[r]);
+      // console.log(response.candles[r]);
     };
-
 
   });  
 
-
-//   }); 
 };
 
 
